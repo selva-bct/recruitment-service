f6c3ce6b5799f09df79149ba1b36cf82c60595e7
selva-bct <selvanathan.v@bahwancybertek.com>
2022-05-23 14:50:24 +0530

    Added Waitlist model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
 export * from './user.model';
 export * from './user-credentials.model';
 
+export * from './waitlist.model';
---
+++ src/models/waitlist.model.ts
@@ -0,0 +1,63 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Waitlist extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  waitlistId?: string;
+
+  @property({
+    type: 'string',
+    index: {
+      unique: true,
+    },
+    jsonSchema: {
+      format: 'email',
+      transform: ['toLowerCase'],
+      errorMessage: {
+        pattern: 'Invalid Email',
+      },
+    },
+  })
+  email?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  firstName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  lastName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+    index: {
+      unique: true,
+    },
+  })
+  phone: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  countryCode: string;
+
+  constructor(data?: Partial<Waitlist>) {
+    super(data);
+  }
+}
+
+export interface WaitlistRelations {
+  // describe navigational properties here
+}
+
+export type WaitlistWithRelations = Waitlist & WaitlistRelations;
