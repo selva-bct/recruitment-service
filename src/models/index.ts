91b8dc7d5ea64e4642479b4e031ed29bbdc22578
selva-bct <selvanathan.v@bahwancybertek.com>
2022-05-23 15:39:10 +0530

    Added User Device Info Model

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,3 +4,4 @@ export * from './user-credentials.model';
 export * from './waitlist.model';
 export * from './role.model';
 export * from './role-mapping.model';
+export * from './user-device-info.model';
---
+++ src/models/user-device-info.model.ts
@@ -0,0 +1,52 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class UserDeviceInfo extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  userDeviceId?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  os: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  os_version: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  deviceType: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  deviceName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  deviceCategory?: string;
+
+
+  constructor(data?: Partial<UserDeviceInfo>) {
+    super(data);
+  }
+}
+
+export interface UserDeviceInfoRelations {
+  // describe navigational properties here
+}
+
+export type UserDeviceInfoWithRelations = UserDeviceInfo & UserDeviceInfoRelations;
