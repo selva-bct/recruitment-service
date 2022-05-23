1188145e66ceaf48598bc667fdb0d60d53a145c5
selva-bct <selvanathan.v@bahwancybertek.com>
2022-05-23 14:44:10 +0530

    Added User Credential Model

---
+++ src/models/user-credentials.model.ts
@@ -0,0 +1,36 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+// @model({settings: {strict: false}})
+export class UserCredentials extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  userId: string;
+  // [prop: string]: any;
+
+
+  constructor(data?: Partial<UserCredentials>) {
+    super(data);
+  }
+}
+
+export interface UserCredentialsRelations {
+  // describe navigational properties here
+}
+
+export type UserCredentialsWithRelations = UserCredentials & UserCredentialsRelations;
